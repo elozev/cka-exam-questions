7ad2a3be68667d58e4d698c7eed8035fcabf30f3
Emil Lozev <em.lozev@gmail.com>
2023-12-02 13:06:45 +0000

    feat: solution q4

---
+++ itexams.com/q4/solution.sh
@@ -0,0 +1,23 @@
+kubectl -n kube-system exec -it etcd-sandbox-cp-0 -- sh -c "ETCDCTL_API=3 \
+ETCDCTL_CACERT=/etc/kubernetes/pki/etcd/ca.crt \
+ETCDCTL_CERT=/etc/kubernetes/pki/etcd/server.crt \
+ETCDCTL_KEY=/etc/kubernetes/pki/etcd/server.key \
+etcdctl endpoint health"
+
+kubectl -n kube-system exec -it etcd-sandbox-cp-0 -- sh -c "ETCDCTL_API=3 \
+ETCDCTL_CACERT=/etc/kubernetes/pki/etcd/ca.crt \
+ETCDCTL_CERT=/etc/kubernetes/pki/etcd/server.crt \
+ETCDCTL_KEY=/etc/kubernetes/pki/etcd/server.key \
+etcdctl --endpoints=https://127.0.0.1:2379 member list"
+
+kubectl -n kube-system exec -it etcd-sandbox-cp-0 -- sh -c "ETCDCTL_API=3 \
+ETCDCTL_CACERT=/etc/kubernetes/pki/etcd/ca.crt \
+ETCDCTL_CERT=/etc/kubernetes/pki/etcd/server.crt \
+ETCDCTL_KEY=/etc/kubernetes/pki/etcd/server.key \
+etcdctl --endpoints=https://127.0.0.1:2379 snapshot save /var/lib/etcd/snapshot.db"
+
+kubectl -n kube-system exec -it etcd-sandbox-cp-0 -- sh -c "ETCDCTL_API=3 \
+ETCDCTL_CACERT=/etc/kubernetes/pki/etcd/ca.crt \
+ETCDCTL_CERT=/etc/kubernetes/pki/etcd/server.crt \
+ETCDCTL_KEY=/etc/kubernetes/pki/etcd/server.key \
+etcdctl --endpoints=https://127.0.0.1:2379 snapshot restore /var/lib/etcd/snapshot.db"
