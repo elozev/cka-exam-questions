006fb3615865a857483c96faace5af8ca929b51c
Emil Lozev <em.lozev@gmail.com>
2023-12-03 13:09:54 +0000

    feat: q6

---
+++ itexams.com/q6/solution.sh
@@ -0,0 +1,15 @@
+kubectl create deploy front-end --image=nginx
+
+kubectl patch deploy front-end --patch='{ "spec": { "template": { "spec": { "containers": [{ "" }] } } } }'
+
+kubectl edit deploy front-end
+
+## add containerPort: 80
+```
+   containers:
+    - image: nginx
+      name: nginx
+      ports:
+      - containerPort: 80
+        name: http
+```
