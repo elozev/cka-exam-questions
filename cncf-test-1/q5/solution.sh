67f471c3cb29d77b7b92ec672ce7d04a58e0ecb1
Emil Lozev <em.lozev@gmail.com>
2023-12-06 21:53:41 +0000

    feat: q5

---
+++ cncf-test-1/q5/solution.sh
@@ -0,0 +1,4 @@
+kubectl get pods --all-namespaces --sort-by=metadata.creationTimestamp > /opt/course/5/find_pods.sh
+
+kubectl get pods --all-namespaces --sort-by=metadata.uuid > /opt/course/5/find_pods_uid.sh
+
