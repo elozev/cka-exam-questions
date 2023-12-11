8297e890abbc2ba1a8f993a85d8bc849632e5d37
Emil Lozev <em.lozev@gmail.com>
2023-12-11 23:14:32 +0000

    feat: q17

--- cncf-test-1/q17/solution.sh
+++ cncf-test-1/q17/solution.sh
@@ -1,1 +1,7 @@
-kubectl get events -A --sort-by="{.metadata.creationTimestamp}"
+kubectl run tigers-reunite --image=httpd:2.4.41-alpine --labels "pod=container,container=pod" --dry-run=client -oyaml > pod.yaml
+
+crictl pods
+
+crictl ps
+
+crictl logs 4b8e3b59be74c
---
+++ cncf-test-1/q18/solution.sh
@@ -0,0 +1,3 @@
+kubectl api-resources --namespaced=true > /opt/course/16/resources.txt
+
+kubectl get roles -A (--all-namespaces) > /opt/course/16/crowded-namespace.txt
